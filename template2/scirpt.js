afa495d022d37e132894fad01d63ca6e1eaebed1
qrbnmhdyv <mehdizade948@gmail.com>
2017-07-16 13:11:44 +0400

    Add files via upload

---
+++ template2/scirpt.js
@@ -0,0 +1,8 @@
+var a=document.createElement("div")
+   document.body.appendChild(a);
+   a.style.width="660px";
+   a.style.height="500";
+   a.style.background="blue"
+   a.style.position="absolute"
+   a.style.display="inline-block"
+  
